7532cb527aa9961964efc0da3ba252d5e9803224
Sinat <kevinthuybui@gmail.com>
2018-10-25 23:15:56 -0700

    Updated TokenFactory in Tokens.jar Finished TokenStreamReader.

--- src/TokenStreamReader.java
+++ src/TokenStreamReader.java
@@ -1,31 +1,104 @@
 import Token.Token;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.InputMismatchException;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * This class creates tokens using the output from our lexer.
+ */
 
 public class TokenStreamReader extends InputStreamReader {
+    /**
+     * This regex Pattern will match our token output and groups the values.
+     */
+    
+    private static final Pattern statePattern =
+            Pattern.compile("Tok: *(\\d*) line= *(\\d*) str= \"(.*)\"(( int= *(\\d*))| float= *(\\d*.\\d*))?");
+    
+    /**
+     * This Matcher uses our Pattern to find the values. We initialize it with an empty string to reuse later.
+     */
+    
+    private Matcher tokenMatcher = statePattern.matcher("");
+    /**
+     * This is the buffer for our peek function.
+     */
+    int buffer;
+    
+    /**
+     * Default constructor
+     * @param in
+     */
     TokenStreamReader(InputStream in) {
         super(in);
     }
     
-    public Token getNextToken() throws InputMismatchException{
-        State state = getState();
-        int lineNumber = getLineNumber();
-        String tokenValue = getTokenValue();
-        return TokenFactory.createToken(state, lineNumber, tokenValue);
+    /**
+     *
+     * @return
+     * @throws TokenInputMalformedException
+     * @throws IOException
+     */
+    
+    public Token getNextToken() throws TokenInputMalformedException, IOException {
+        StringBuilder nextToken = new StringBuilder();
+        
+        char nextChar = (char) read();
+        while(nextChar != '\n'){
+            nextToken.append(nextChar);
+            
+            int nextInt = read();
+            if (nextInt == -1) break;
+            else nextChar = (char) nextInt;
+        }
+        
+        String tokenString = nextToken.toString();
+        tokenMatcher.reset(tokenString);
+        if (tokenMatcher.find()) {
+            State state = getState(Integer.parseInt(tokenMatcher.group(1)));
+            int lineNumber = Integer.parseInt(tokenMatcher.group(2));
+            String tokenValue = tokenMatcher.group(3);
+            return TokenFactory.createToken(state, lineNumber, tokenValue);
+        } else {
+            throw new TokenInputMalformedException();
+        }
     }
     
-    //TODO Implement token parsing
-    private String getTokenValue() {
-        return null;
-    }
-    
-    private int getLineNumber() {
-        return 0;
-    }
-    
-    private State getState() {
-        return null;
+    private State getState(int stateValue) {
+        State state;
+        Optional<State> potentialState = State.valueOf(stateValue);
+        if (potentialState.isPresent()) {
+            state = potentialState.get();
+        }
+        else {
+            throw new InputMismatchException();
+        }
+        switch (state){
+            case KPROG:
+            case KMAIN:
+            case KFCN:
+            case KCLASS:
+            case KFLOAT:
+            case KINT:
+            case KSTRING:
+            case KIF:
+            case KELSEIF:
+            case KELSE:
+            case KWHILE:
+            case KINPUT:
+            case KPRINT:
+            case KNEW:
+            case KRETURN:
+            case KVAR:
+                return State.ID;
+            default:
+                break;
+        }
+        return state;
     }
 }
